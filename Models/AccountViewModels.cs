49561b066d9b8f119d46f46cfb60f74c5dd27437
Nikoaly Trifonov <koksi75@gmail.com>
2016-12-06 04:33:11 +0200

    Първа промяна

---
+++ Models/AccountViewModels.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BurgasTeamBlog.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Ел.Поща")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Код")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Запомни този браузър?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Ел.Поща")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Ел.Поща")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомни ме?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Име и фамилия")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Рождена дата")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Пол [мъж / жена]")]
+        public String Gender { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Ел.Поща")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Паролата трябва да е от 1 до 100 символа", MinimumLength = 1)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Потвърди парола")]
+        [Compare("Password", ErrorMessage = "Паролата и потвърждението на паролата не съвпадат.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Ел.Поща")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Паролата трябва да е от 1 до 100 символа", MinimumLength = 1)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Потвърди парола")]
+        [Compare("Password", ErrorMessage = "Паролата и потвърждението на паролата не съвпадат.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Ел.Поща")]
+        public string Email { get; set; }
+    }
+}
